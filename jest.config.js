3b47ee9aa9158c2644a7dcc2857d0bc15cd29cef
Petar Ivaylov Ivanov <pepincho95@gmail.com>
2022-11-30 17:14:06 +0200

    configure 100% test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
-const nextJest = require('next/jest')
+const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
-})
+});
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
@@ -15,7 +15,17 @@ const customJestConfig = {
     '^@/pages/(.*)$': '<rootDir>/pages/$1',
   },
   testEnvironment: 'jest-environment-jsdom',
-}
+
+  collectCoverageFrom: ['**/*.{ts,tsx}', '!pages/_app.tsx'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+};
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig)
+module.exports = createJestConfig(customJestConfig);
