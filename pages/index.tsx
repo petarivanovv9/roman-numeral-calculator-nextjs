01f30a476f9dd4452f7f790a17a2fe1b1190b930
Petar Ivaylov Ivanov <pepincho95@gmail.com>
2022-11-30 19:22:05 +0200

    add dummy input field & button with styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 export default function Home() {
+  const romanNumber = null;
+
   return (
     <div className="h-full bg-blue-800">
       {/* Container */}
@@ -10,7 +12,49 @@ export default function Home() {
             <div className="w-full">
               <h1 className="mb-12 text-white text-center">Roman Calculator</h1>
 
-              <div className="bg-white rounded-lg p-4">FORM</div>
+              {/* Main Section */}
+              <main className="bg-white rounded-lg p-4">
+                <form>
+                  {/* Input Field */}
+                  <div className="md:p-4">
+                    <label className="block text-sm font-medium text-gray-700">
+                      Enter natural number:
+                    </label>
+                    <input
+                      type="text"
+                      name="natural"
+                      id="natural"
+                      className="placeholder-gray-400 leading-6 w-full px-3 py-2
+                      border border-gray-300 rounded-md shadow-sm
+                      transition ease-in-out duration-100
+                      focus:ring-indigo-500 focus:border-indigo-500 focus:outline-none
+                      focus:ring-1 focus:ring-offset-0"
+                      placeholder="Ex: 123"
+                      autoFocus
+                    />
+                  </div>
+                  {/* End Input Field */}
+
+                  {/* Button */}
+                  <div className="md:p-4">
+                    <button
+                      type="submit"
+                      className="border border-transparent rounded text-white ring-gray-200 m-0.5 px-2.5 py-1.5
+                      transition-all ease-in duration-75 hover:bg-gray-100 hover:shadow-sm
+                      focus:outline-none focus:ring-2 focus:ring-offset-2
+                      bg-indigo-500 text-white ring-indigo-500 hover:bg-indigo-600 w-full"
+                    >
+                      Convert
+                    </button>
+                  </div>
+                  {/* End Button */}
+                </form>
+
+                <div className="md:p-4">
+                  Result: <span>{romanNumber ?? 'N/A'}</span>
+                </div>
+              </main>
+              {/* End Section */}
             </div>
           </div>
           {/* End Col */}
