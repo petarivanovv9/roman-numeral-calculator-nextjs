88eec1f8dd3122b028d411e279068ae5a643cb2a
Petar Ivaylov Ivanov <pepincho95@gmail.com>
2022-11-30 18:50:41 +0200

    add initial responsive home page without input

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,23 @@
 export default function Home() {
-  return <div>Roman Calculator</div>;
+  return (
+    <div className="h-full bg-blue-800">
+      {/* Container */}
+      <div className="max-w-full h-full px-4">
+        {/* Row */}
+        <div className="flex flex-wrap h-full justify-center">
+          {/* Col */}
+          <div className="flex flex-row items-center xl:w-4/12 lg:w-6/12 sm:w-8/12 w-full">
+            <div className="w-full">
+              <h1 className="mb-12 text-white text-center">Roman Calculator</h1>
+
+              <div className="bg-white rounded-lg p-4">FORM</div>
+            </div>
+          </div>
+          {/* End Col */}
+        </div>
+        {/* End Row */}
+      </div>
+      {/* End Container */}
+    </div>
+  );
 }
