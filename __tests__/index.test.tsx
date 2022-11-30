57da54189408b82933d7f83336d1a8e582486e50
Petar Ivaylov Ivanov <pepincho95@gmail.com>
2022-11-30 20:09:16 +0200

    add conversion functionality into component

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,10 +1,22 @@
-import { render, screen } from '@testing-library/react';
-import Home from '@/pages/index';
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import Home, { testIds } from '@/pages/index';
 
 describe('Home', () => {
   it('should match snapshot', () => {
     const { container } = render(<Home />);
 
     expect(container).toMatchSnapshot();
+  });
+
+  it('should convert `317` to `CCCXVII`', async () => {
+    render(<Home />);
+
+    await userEvent.type(screen.getByTestId(testIds.naturalInput), '317');
+
+    await userEvent.click(screen.getByTestId(testIds.submit));
+
+    expect(await screen.findByText(/CCCXVII/)).toBeInTheDocument();
   });
 });
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,31 @@
+import { Formik, Field } from 'formik';
+import { useState } from 'react';
+
+import { naturalToRoman } from './utils';
+
+export const testIds = {
+  naturalInput: 'natural-input-field',
+  submit: 'submit-button',
+  result: 'result-text',
+};
+
+interface FormValues {
+  natural: string;
+}
+
+const initialValues: FormValues = {
+  natural: '',
+};
+
 export default function Home() {
-  const romanNumber = null;
+  const [romanNumber, setRomanNumber] = useState<string | null>(null);
+
+  const handleFormSubmit = (values: FormValues) => {
+    const natural = parseInt(values.natural);
+    const converted = naturalToRoman(natural);
+
+    setRomanNumber(converted);
+  };
 
   return (
     <div className="h-full bg-indigo-800">
@@ -14,44 +40,56 @@ export default function Home() {
 
               {/* Main Section */}
               <main className="bg-white rounded-lg p-4">
-                <form>
-                  {/* Input Field */}
-                  <div className="md:p-4">
-                    <label className="block text-sm font-medium text-gray-700">
-                      Enter natural number:
-                    </label>
-                    <input
-                      type="text"
-                      name="natural"
-                      id="natural"
-                      className="placeholder-gray-400 leading-6 w-full px-3 py-2
-                      border border-gray-300 rounded-md shadow-sm
-                      transition ease-in-out duration-100
-                      focus:ring-indigo-500 focus:border-indigo-500 focus:outline-none
-                      focus:ring-1 focus:ring-offset-0"
-                      placeholder="Ex: 123"
-                      autoFocus
-                    />
-                  </div>
-                  {/* End Input Field */}
+                <Formik
+                  enableReinitialize
+                  initialValues={initialValues}
+                  onSubmit={handleFormSubmit}
+                >
+                  {(formikProps) => (
+                    <form onSubmit={formikProps.handleSubmit}>
+                      {/* Input Field */}
+                      <div className="md:p-4">
+                        <label
+                          htmlFor="natural"
+                          className="block text-sm font-medium text-gray-700"
+                        >
+                          Enter natural number:
+                        </label>
+                        <Field
+                          name="natural"
+                          placeholder="Ex: 123"
+                          data-testid={testIds.naturalInput}
+                          className="placeholder-gray-400 leading-6 w-full px-3 py-2
+                          border border-gray-300 rounded-md shadow-sm
+                          transition ease-in-out duration-100
+                          focus:ring-indigo-500 focus:border-indigo-500 focus:outline-none
+                          focus:ring-1 focus:ring-offset-0"
+                          autoFocus
+                        />
+                      </div>
+                      {/* End Input Field */}
 
-                  {/* Button */}
-                  <div className="md:p-4">
-                    <button
-                      type="submit"
-                      className="border border-transparent rounded text-white ring-gray-200 m-0.5 px-2.5 py-1.5
-                      transition-all ease-in duration-75 hover:bg-gray-100 hover:shadow-sm
-                      focus:outline-none focus:ring-2 focus:ring-offset-2
-                      bg-indigo-500 text-white ring-indigo-500 hover:bg-indigo-600 w-full"
-                    >
-                      Convert
-                    </button>
-                  </div>
-                  {/* End Button */}
-                </form>
+                      {/* Button */}
+                      <div className="md:p-4">
+                        <button
+                          type="submit"
+                          data-testid={testIds.submit}
+                          className="border border-transparent rounded text-white ring-gray-200 m-0.5 px-2.5 py-1.5
+                          transition-all ease-in duration-75 hover:bg-gray-100 hover:shadow-sm
+                          focus:outline-none focus:ring-2 focus:ring-offset-2
+                          bg-indigo-500 text-white ring-indigo-500 hover:bg-indigo-600 w-full"
+                        >
+                          Convert
+                        </button>
+                      </div>
+                      {/* End Button */}
+                    </form>
+                  )}
+                </Formik>
 
                 <div className="md:p-4">
-                  Result: <span>{romanNumber ?? 'N/A'}</span>
+                  Result:{' '}
+                  <span className="font-bold">{romanNumber || 'N/A'}</span>
                 </div>
               </main>
               {/* End Section */}
